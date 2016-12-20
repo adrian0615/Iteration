befe466e2d62170cfe6666d40acdcd25402743f7
Adrian McDaniel <adriandmcdaniel@gmail.com>
2016-12-20 17:13:18 -0500

    First Commit

---
+++ createTake.swift
@@ -0,0 +1,18 @@
+//Create a function createTake which accepts an Int and returns a function that takes the first n items from any sequence and produces an Array.
+
+//func createTake(n: Int, rArray: Array) -> Array {
+
+
+
+
+
+
+//func createTake< Foo : Sequence>(sequece: Foo) {
+    
+//}
+//
+
+//let firstN = MySequence
+//randomSequence = [1, 2, 3, 4, 5]
+//func firstN -> takes items from random sequence and returns the first N items
+//func createTake(n: Int) -> firstN
